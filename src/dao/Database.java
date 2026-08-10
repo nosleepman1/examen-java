d4c64eb6de8d369ed4ecd4147be074df45c60a89
nosleepman1 <abdallahdiouf.dev@gmail.com>
2026-08-10 16:22:00 +0000

    fix(dao): ajuste le mot de passe MySQL local

--- src/dao/Database.java
+++ src/dao/Database.java
@@ -12,7 +12,7 @@ public class Database {
 
     private static final String URL = "jdbc:mysql://localhost:3306/joj_dakar2026?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "root";
 
     private Database() {
     }
